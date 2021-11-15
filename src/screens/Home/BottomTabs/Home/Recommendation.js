f471b36088054cb44ba020848babb36f5fb646bd
saniya384 <79018100+saniya384@users.noreply.github.com>
2021-11-15 04:18:40 -0800

    recommendation

--- src/screens/Home/BottomTabs/Home/Recommendation.js
+++ src/screens/Home/BottomTabs/Home/Recommendation.js
@@ -31,7 +31,8 @@ const Recommendation = (props) => {
                     bfaRecommendation.length > 0 ? 
                     bfaRecommendation.map((item, index) => {
                         return (
-                            <TouchableOpacity onPress={()=> props.navigation.navigate(routeName.DishDetails,{dish:item})}>
+                            <TouchableOpacity onPress={()=> props.navigation.navigate(routeName.DISH_DETAIL,{dish:url})}>
+                            
                             <View  style={{ width: wp(26), height: hp(18), borderRadius: 3, marginHorizontal: 5, overflow: 'hidden', flexDirection: 'row' }}>
                                 <ImageBackground imageStyle={{opacity:.5}} style={{ flex: 1, padding: 5, overflow: 'hidden', justifyContent: 'flex-end',backgroundColor: 'rgba(0,0,0,1)' }} source={{uri: item.resLogo}} >
                                     <ResponsiveText fontFamily="Regular" size={3} margin={[0,0,5,0]} color={colors.white}>{item.dishName}</ResponsiveText>
