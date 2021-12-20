f4fdfc80226affbf855989b4364e9ecda1157d3b
SulemanMirz <sulemug@gmail.com>
2021-12-20 22:46:06 +0500

    wallet icons

--- src/screens/Home/Wallet/Wallet.js
+++ src/screens/Home/Wallet/Wallet.js
@@ -3,12 +3,20 @@ import React from 'react'
 import { StyleSheet, Text, View,Image } from 'react-native'
 import { colors } from '../../../constants/colorsPallet'
 
+import dollar from '../../../assets/icons/cowries.png'
+
 import _1BCoin from '../../../assets/BCoins/1_BCOIN.png'
 import _5BCoin from '../../../assets/BCoins/5_BCOINS.png'
 import _10BCoin from '../../../assets/BCoins/10_BCOINS.png'
 import _20BCoin from '../../../assets/BCoins/20_BCOINS.png'
 import _50BCoin from '../../../assets/BCoins/50_BCOINS.png'
 import _100BCoin from '../../../assets/BCoins/100_HUNDRED_BCOINS.png'
+
+import _1BCent from '../../../assets/BCents/1_BCENT.png'
+import _5BCent from '../../../assets/BCents/5_BCENTS.png'
+import _10BCent from '../../../assets/BCents/10_BCENTS.png'
+import _20BCent from '../../../assets/BCents/20_BCENTS.png'
+import _50BCent from '../../../assets/BCents/50_BCENTS.png'
 
 const Wallet = (props) => {
 
@@ -20,15 +28,15 @@ const Wallet = (props) => {
             <Text style={{color:'white',fontSize:70}} >10.00</Text>
             <View style={{flexDirection:'row',paddingBottom:20}} >
                 <View style={{alignItems:'center'}}>
-                    <Image source={_1BCoin} style={styles.image}/>
+                    <Image source={dollar} style={styles.image}/>
                     <Text style={{color:'white'}}>History</Text>
                 </View>
                 <View style={{alignItems:'center'}}>
-                    <Image source={_1BCoin} style={styles.image}/>
+                    <Image source={dollar} style={styles.image}/>
                     <Text style={{color:'white'}} >Transfer</Text>
                 </View>
                 <View style={{alignItems:'center'}}>
-                    <Image source={_1BCoin} style={styles.image}/>
+                    <Image source={dollar} style={styles.image}/>
                     <Text style={{color:'white'}} >Top-Up</Text>
                 </View>
 
@@ -36,7 +44,7 @@ const Wallet = (props) => {
 
             <View style={{alignItems:'center', paddingVertical:20, borderWidth:1, borderColor:'white', width:'100%'}} >
 
-                <Text style={{color:'white'}}>BND $1 = 1 Bitcoin </Text>
+                <Text style={{color:'white',fontSize:20}}>BND $1 = 1 Bcoin </Text>
 
             </View>
             
@@ -62,6 +70,15 @@ const Wallet = (props) => {
                     <Image source={_100BCoin} style={styles.image2}/>
                 </View>
             </View> 
+            <View style={{flexDirection:'row',paddingTop:20,paddingHorizontal:2,justifyContent:'flex-end' }} >
+                <Image source={_1BCent} style={{marginHorizontal:-20, height:100,width:100}} />
+                <Image source={_5BCent} style={{marginHorizontal:-18,height:105,width:105}} />
+                <Image source={_10BCent} style={{marginHorizontal:-15,height:110,width:110}} />
+                <Image source={_20BCent} style={{marginHorizontal:-10,height:115,width:115}} />
+                <Image source={_50BCent} style={{marginHorizontal:-8,height:120,width:120}} />
+            </View>
+            <Text style={{color:'white',fontSize:19}}>Your BCoin Balance is:</Text>
+            <Text style={{color:'white',fontSize:70}} >$10.00</Text>
         </View>
     )
 }
@@ -75,8 +92,8 @@ const styles = StyleSheet.create({
         alignItems:'center',
     },
     image:{
-        height:100,
-        width:100,
+        height:80,
+        width:80,
         paddingHorizontal:5,
         borderRadius:5,
         marginHorizontal:5
