a555b39119b1a96c5890c14a4ece082dc6fbf4d5
saniya384 <79018100+saniya384@users.noreply.github.com>
2021-12-30 14:29:46 +0500

    change

--- src/screens/Home/BottomTabs/Home/AdvertisementBanner.js
+++ src/screens/Home/BottomTabs/Home/AdvertisementBanner.js
@@ -30,7 +30,7 @@ const AdvertisementBanner = ({ navigation }) => {
           showsButtons={false}
           autoplay={true}
           autoplayTimeout={3}
-          loadMinimalSize={3}
+          removeClippedSubviews={true}
           activeDot={
             <View
               style={{
