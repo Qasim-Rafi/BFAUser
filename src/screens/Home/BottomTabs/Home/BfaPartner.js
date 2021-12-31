d902fd373fdaa36cf02ca9a3597c225024f92e97
saniya384 <79018100+saniya384@users.noreply.github.com>
2021-12-31 15:47:07 +0500

    changing

--- src/screens/Home/BottomTabs/Home/BfaPartner.js
+++ src/screens/Home/BottomTabs/Home/BfaPartner.js
@@ -5,6 +5,8 @@ import {
   View,
   ScrollView,
   TouchableOpacity,
+  Modal,
+  Button
 } from 'react-native';
 import ResponsiveText from '../../../../components/RnText';
 
@@ -23,8 +25,10 @@ import urls from '../../../../redux/lib/urls';
 import { hp } from '../../../../helpers/Responsiveness';
 import AsyncStorage from '@react-native-community/async-storage';
 import Icon from '../../../../components/Icon';
-import { Linking } from 'react-native';
 import { getBfaPartners } from '../../../../redux/actions/user.actions';
+
+// import { WebView } from 'react-native-webview's
+
 const BfaPartner = ({ props }) => {
   const dispatch = useDispatch();
   const loading = useSelector(
@@ -33,6 +37,8 @@ const BfaPartner = ({ props }) => {
 
   // console.log('loading', loading);
   const [moreData, setMoreData] = React.useState(false);
+  const [modalVisble, setModalVisible] = React.useState(false)
+
   let bfaPartners = useSelector(state => state.appReducers.bfaPartners.data);
   const [title, setTitle] = React.useState(
     bfaPartners.length <= 6 ? 'Less' : 'More',
@@ -41,6 +47,7 @@ const BfaPartner = ({ props }) => {
   // console.log('BFA Partners: ', bfaPartners);
   const images = [];
   const lessImages = [];
+  const siteAdd = []
   if (loading === false) {
     bfaPartners.map(item => {
       var img = item.imageDataB;
@@ -53,6 +60,14 @@ const BfaPartner = ({ props }) => {
       //  console.log('All Images: ',images);
     });
   }
+
+  if (loading === false) {
+    bfaPartners.map((item, index) => {
+      siteAdd.push(bfaPartners[index].siteUrl)
+    })
+  }
+  // console.log(siteAdd);
+
   if (loading === false) {
     for (var i = 0; i < 6; i++) {
       var img = bfaPartners[i].imageDataB;
@@ -63,6 +78,25 @@ const BfaPartner = ({ props }) => {
       }
     }
   }
+
+  console.log('----------------------');
+  console.log(siteAdd);
+  console.log('----------------------')
+
+  const modalView = (index, visibilty) => (
+    <View>
+      <Modal visible={true} style={{ height: '80%', width: '80%' }} >
+        <View style={{ flex: 1, justifyContent: 'center' }} >
+          {console.log(siteAdd[index])}
+          <WebView
+            source={{ uri: siteAdd[index] }}
+          // style={{height:'80%',width:'80%'}}
+          />
+          <Button title={'Close'} onPress={() => setModalVisible(false)} />
+        </View>
+      </Modal>
+    </View>
+  )
 
   useEffect(() => {
     dispatch(getBfaPartners(6))
@@ -113,11 +147,9 @@ const BfaPartner = ({ props }) => {
         {images.length > 0
           ? title === 'More'
             ? lessImages.map((url, index) => {
-
+              console.log(siteAdd[index])
               return (
                 // <Icon source={url} size={35} borderRadius={5} />
-
-
                 <View
                   style={{
                     backgroundColor: colors.white,
@@ -125,20 +157,22 @@ const BfaPartner = ({ props }) => {
                     marginRight: 5,
                     marginVertical: 3,
                   }}>
-                  <Icon
-                    source={{
-                      uri: url,
-                    }}
-                    // source={url}
+                  <TouchableOpacity onPress={() => modalView(index, true)} >
+                    <Icon
+                      source={{
+                        uri: url,
+                      }}
+                      // source={url}
 
-                    size={55}
-                    borderRadius={5}
-                  />
+                      size={55}
+                      borderRadius={5}
+                    />
+                  </TouchableOpacity>
                 </View>
-
               );
             })
             : images.map((url, index) => {
+              console.log(bfaPartners[index])
               return (
                 // <Icon source={url} size={35} borderRadius={5} />
                 <View
@@ -148,21 +182,25 @@ const BfaPartner = ({ props }) => {
                     marginRight: 5,
                     marginVertical: 3,
                   }}>
-                  <Icon
-                    source={{
-                      uri: url,
-                    }}
-                    // source={url}
+                  <TouchableOpacity onPress={() => modalView(index, true)} >
+                    <Icon
+                      source={{
+                        uri: url,
+                      }}
+                      // source={url}
 
-                    size={55}
-                    borderRadius={5}
-                  />
+                      size={55}
+                      borderRadius={5}
+                    />
+
+                  </TouchableOpacity>
                 </View>
               );
             })
           : undefined}
 
       </View>
+
 
     </View>
   );
