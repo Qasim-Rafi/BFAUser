0b3241a42be9c4ce52056784d48729176676e79d
Altaf <itxaltaf789@gmail.com>
2022-01-11 14:54:35 +0500

    Add favorite,get all favorite

--- src/screens/Home/DishDetails/DishDescription.js
+++ src/screens/Home/DishDetails/DishDescription.js
@@ -1,20 +1,30 @@
 import React from 'react';
-import { View, Text, ScrollView, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import AdvertisementBanner from '../BottomTabs/Home/AdvertisementBanner';
 import ImageHeader from '../BottomTabs/Home/ImageHeader';
-import { colors } from '../../../constants/colorsPallet';
+import {colors} from '../../../constants/colorsPallet';
 const text = 'Description';
 import Icon from '../../../components/Icon';
 import ResponsiveText from '../../../components/RnText';
-import { globalPath } from '../../../constants/globalPath';
-import { wp } from '../../../helpers/Responsiveness';
+import {globalPath} from '../../../constants/globalPath';
+import {wp} from '../../../helpers/Responsiveness';
 import StaticMap from '../../../components/StaticMap';
-import { useDispatch, useSelector } from 'react-redux';
-import { addFavorite, onRemoveFavorite } from '../../../redux/actions/user.actions';
+import {useDispatch, useSelector} from 'react-redux';
+import {
+  addFavorite,
+  onRemoveFavorite,
+} from '../../../redux/actions/user.actions';
 
 export default function DishDescription(props) {
-  const dispatch = useDispatch()
-  const favData = useSelector(state => state.appReducers.favorite.data,)
+  const dispatch = useDispatch();
+  const favData = useSelector(state => state.appReducers.favorite.data);
 
   // console.log(removefavorite,'hhh')
   return (
@@ -29,10 +39,12 @@ export default function DishDescription(props) {
           }}>
           <View style={styles.priceDesc}>
             <ResponsiveText size={4} color={colors.white}>
-              Salman Sashimi Sushi
+              {props.item.titleD == undefined
+                ? props.item.titleA
+                : props.item.titleD}
             </ResponsiveText>
             <ResponsiveText size={3.7} color={colors.grey}>
-              1-Lotus Resturant
+              {props.item.titleR}
             </ResponsiveText>
             <ResponsiveText size={4} color={colors.yellow}>
               $10.00
@@ -45,9 +57,7 @@ export default function DishDescription(props) {
           />
         </View>
 
-        <View>
-          {/* <Image style={{ width: 30, height: 25 }} /> */}
-        </View>
+        <View>{/* <Image style={{ width: 30, height: 25 }} /> */}</View>
       </View>
       <View
         style={{
@@ -57,54 +67,59 @@ export default function DishDescription(props) {
           borderBottomColor: colors.grey,
           borderBottomWidth: 1,
         }}>
-        <TouchableOpacity style={{ alignItems: 'center' }} onPress={() => {
-          !favData.some(o => o.id === props.item.id)
-            ? dispatch(addFavorite(props.item)) : dispatch(onRemoveFavorite(props.item))
-        }}
-
-        >
-          <Icon source={favData.some(o => o.id === props.item.id) ? globalPath.F_HEART : globalPath.HEART}
-
+        <TouchableOpacity
+          style={{alignItems: 'center'}}
+          onPress={() => {
+            !favData.some(o => o.id === props.item.id)
+              ? dispatch(addFavorite(props.item))
+              : dispatch(onRemoveFavorite(props.item));
+          }}>
+          <Icon
+            source={
+              favData.some(o => o.id === props.item.id)
+                ? globalPath.F_HEART
+                : globalPath.HEART
+            }
           />
           <ResponsiveText top={5} color={colors.yellow}>
             Favourite
           </ResponsiveText>
         </TouchableOpacity>
-        <View style={{ alignItems: 'center' }}>
+        <View style={{alignItems: 'center'}}>
           <Icon source={globalPath.LOCATION} />
           <ResponsiveText top={5} color={colors.yellow}>
             Go To
           </ResponsiveText>
         </View>
-        <View style={{ alignItems: 'center' }}>
+        <View style={{alignItems: 'center'}}>
           <Icon source={globalPath.CONTACT} />
           <ResponsiveText top={5} color={colors.yellow}>
             Contact
           </ResponsiveText>
         </View>
-        <View style={{ alignItems: 'center' }}>
+        <View style={{alignItems: 'center'}}>
           <Icon source={globalPath.MORE_LOGO} />
           <ResponsiveText top={5} color={colors.yellow}>
             More
           </ResponsiveText>
         </View>
       </View>
-      <View style={{ padding: 20, }}>
+      <View style={{padding: 20}}>
         <ResponsiveText fontFamily="Regular" size={4} color={colors.white}>
-          Dish Description
+          {props.item.titleA == undefined
+            ? props.item.titleD
+            : props.item.titleA}
         </ResponsiveText>
         <ResponsiveText
           top={5}
           fontFamily="Regular"
           size={3.5}
           color={colors.grey}>
-          Lorem Ipsum is simply dummy text of the printing.Lorem Ipsum is simply dummy text of the printing.
-          Lorem Ipsum is simply dummy text of the printing.
+          Lorem Ipsum is simply dummy text of the printing.Lorem Ipsum is simply
+          dummy text of the printing. Lorem Ipsum is simply dummy text of the
+          printing.
         </ResponsiveText>
       </View>
-
-
-
     </View>
   );
 }
