93b310c424f49df6cdd988d124b18e120802ec9a
saniya384 <79018100+saniya384@users.noreply.github.com>
2022-01-12 12:00:08 +0500

    show All btn

--- src/screens/Home/DishDetails/MoreFromResturant.js
+++ src/screens/Home/DishDetails/MoreFromResturant.js
@@ -5,19 +5,19 @@ import ResponsiveText from '../../../components/RnText';
 import { advertisementBannerFakeDATA, ourRecommendationFakeDATA } from '../../../constants/mock'
 import { colors } from '../../../constants/colorsPallet'
 import SeeAllButton from '../../../components/SeeAllButton';
-const MoreFromResturant = ({navigation}) => {
+const MoreFromResturant = ({ navigation }) => {
     return (
         <>
             <View style={styles.yourFavoriteHeaderSection}>
-                <ResponsiveText size={4}color={colors.white}>More From Resturant</ResponsiveText>
-                <SeeAllButton navigation={navigation}/>
+                <ResponsiveText size={4} color={colors.white}>More From Resturant</ResponsiveText>
+                {/* <SeeAllButton navigation={navigation}/> */}
             </View>
             <View style={styles.yourFavoriteItemsSection}>
                 <ScrollView showsHorizontalScrollIndicator={false} horizontal>
                     {ourRecommendationFakeDATA.map((url, index) => {
                         return (
                             <View style={{ width: 90, height: 130, marginRight: 5, borderRadius: 3, overflow: 'hidden', flexDirection: 'row' }}>
-                                <ImageBackground style={{ flex: 1, padding: 5, overflow: 'hidden', justifyContent: 'flex-end' }} source={url.url } >
+                                <ImageBackground style={{ flex: 1, padding: 5, overflow: 'hidden', justifyContent: 'flex-end' }} source={url.url} >
                                     <ResponsiveText fontFamily="Regular" size={2.9} color={colors.white}>{url.title}</ResponsiveText>
                                     <ResponsiveText fontFamily="Light" size={2} color={colors.white}>{url.description}</ResponsiveText>
 
