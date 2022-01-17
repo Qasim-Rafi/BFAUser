c0e828b654ad6435a9f411e286e5c520d13a1757
saniya384 <79018100+saniya384@users.noreply.github.com>
2022-01-17 17:07:41 +0500

    signup

--- src/screens/Auth/SignUp/SignUp.js
+++ src/screens/Auth/SignUp/SignUp.js
@@ -7,7 +7,7 @@ import {
   Platform,
   Button,
   TextInput,
-  TouchableOpacity,KeyboardAvoidingView,TouchableWithoutFeedback,Keyboard,
+  TouchableOpacity, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard,
   Text,
   SafeAreaView,
 } from 'react-native';
@@ -198,68 +198,77 @@ export default function Signup({ navigation }) {
   };
 
   return (
-    <KeyboardAvoidingView style={{flex: 1, backgroundColor: colors.black}}  behavior={Platform.OS === "ios" ? "padding" : "height"}>
+    <KeyboardAvoidingView style={{ flex: 1, backgroundColor: colors.black }} behavior={Platform.OS === "ios" ? "padding" : null}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <ScrollView style={{flex: 1}}>
-        <View style={styles.screeninfo}>
-          <Icon source={globalPath.BALI_ICON} size={60} />
-          <ResponsiveText
-            margin={[1, 0, 0, 0]}
-            color={colors.yellow}
-            fontFamily="Regular"
-            size={8}>
-            Sign Up
-          </ResponsiveText>
-          <ResponsiveText margin={[1, 0, 0, 0]} color={colors.white}>
-            Please SignUp to Continue
-          </ResponsiveText>
-        </View>
-        <View style={styles.formArea} >
-
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <Input
-              width={wp(39)}
-              margin={[0, 0, 15, 0]}
-              padding={[0, 0, 0, 25]}
-              onChnageText={text => setFirstName(text)}
-              iconMargin={[0, 10, 0, 0]}
-              placeholder="First name"
-              leftIcon={globalPath.USER_LOGO}
-            />
-            <Input
-              width={wp(39)}
-              onChnageText={text => setLastName(text)}
-              margin={[0, 0, 15, 0]}
-              padding={[0, 0, 0, 25]}
-              iconMargin={[0, 10, 0, 0]}
-              placeholder="Last name"
-              leftIcon={globalPath.USER_LOGO}
-            />
+        <ScrollView style={{ flex: 1 }}>
+          <View style={styles.screeninfo}>
+            <Icon source={globalPath.BALI_ICON} size={60} />
+            <ResponsiveText
+              margin={[1, 0, 0, 0]}
+              color={colors.yellow}
+              fontFamily="Regular"
+              size={8}>
+              Sign Up
+            </ResponsiveText>
+            <ResponsiveText margin={[1, 0, 0, 0]} color={colors.white}>
+              Please SignUp to Continue
+            </ResponsiveText>
           </View>
-          <Input
-            padding={[0, 0, 0, 25]}
-            margin={[0, 0, 15, 0]}
-            iconMargin={[0, 10, 0, 0]}
-            placeholder="User Name"
-            onChnageText={text => setuserName(text)}
-            leftIcon={globalPath.MALE_LOGO}
-          />
-          <Input
-            padding={[0, 0, 0, 25]}
-            iconMargin={[0, 10, 0, 0]}
-            placeholder="Email"
-            onChnageText={text => setEmail(text)}
-            leftIcon={globalPath.EMAIL_LOGO}
-          />
-          <Input
-            margin={[15, 0, 15, 0]}
-            padding={[0, 0, 0, 25]}
-            onChnageText={text => setPhoneNum(text)}
-            iconMargin={[0, 10, 0, 0]}
-            placeholder="Phone"
-            leftIcon={globalPath.PHONE_LOGO}
-          />
-          <Input
+          <View style={styles.formArea} >
+
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <Input
+                width={wp(39)}
+                margin={[0, 0, 15, 0]}
+                padding={[0, 0, 0, 25]}
+                onChnageText={text => setFirstName(text)}
+                iconMargin={[0, 10, 0, 0]}
+                placeholder="First name"
+                leftIcon={globalPath.USER_LOGO}
+              />
+              <Input
+                width={wp(39)}
+                onChnageText={text => setLastName(text)}
+                margin={[0, 0, 15, 0]}
+                padding={[0, 0, 0, 25]}
+                iconMargin={[0, 10, 0, 0]}
+                placeholder="Last name"
+                leftIcon={globalPath.USER_LOGO}
+              />
+            </View>
+            <Input
+              padding={[0, 0, 0, 25]}
+              margin={[0, 0, 15, 0]}
+              iconMargin={[0, 10, 0, 0]}
+              placeholder="User Name"
+              onChnageText={text => setuserName(text)}
+              leftIcon={globalPath.MALE_LOGO}
+            />
+            <Input
+              padding={[0, 0, 0, 25]}
+              iconMargin={[0, 10, 0, 0]}
+              placeholder="Email"
+              onChnageText={text => setEmail(text)}
+              leftIcon={globalPath.EMAIL_LOGO}
+            />
+            <Input
+              margin={[15, 0, 15, 0]}
+              padding={[0, 0, 0, 25]}
+              onChnageText={text => setPhoneNum(text)}
+              iconMargin={[0, 10, 0, 0]}
+              placeholder="Phone"
+              leftIcon={globalPath.PHONE_LOGO}
+            />
+            <Input
+              margin={[0, 0, 15, 0]}
+              padding={[0, 0, 0, 25]}
+              iconMargin={[0, 10, 0, 0]}
+              placeholder="Password"
+              onChnageText={text => setPassword(text)}
+              secureTextEntry
+              leftIcon={globalPath.PASSWORD_LOGO}
+            />
+            {/* <Input
             margin={[0, 0, 15, 0]}
             padding={[0, 0, 0, 25]}
             iconMargin={[0, 10, 0, 0]}
@@ -276,17 +285,8 @@ export default function Signup({ navigation }) {
             onChnageText={text => setPassword(text)}
             secureTextEntry
             leftIcon={globalPath.PASSWORD_LOGO}
-          />
-          <Input
-            margin={[0, 0, 15, 0]}
-            padding={[0, 0, 0, 25]}
-            iconMargin={[0, 10, 0, 0]}
-            placeholder="Password"
-            onChnageText={text => setPassword(text)}
-            secureTextEntry
-            leftIcon={globalPath.PASSWORD_LOGO}
-          />
-          {/* <Input
+          /> */}
+            {/* <Input
             margin={[0, 0, 15, 0]}
             padding={[0, 0, 0, 25]}
             iconMargin={[0, 10, 0, 0]}
@@ -295,75 +295,75 @@ export default function Signup({ navigation }) {
             secureTextEntry
             leftIcon={globalPath.PASSWORD_LOGO}
           /> */}
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            <DropDown data={Gender} height={hp(6)} width={wp(39)} />
-            <View>
-              <View style={{ borderWidth: 2, zIndex: 0, borderRadius: 10 }}>
-                <Text
-                  style={{
-                    fontSize: 7,
-                    position: 'absolute',
-                    zIndex: 1,
-                    top: -5,
-                    marginStart: 9,
-                    color: colors.white,
-                  }}>
-                  Date of birth
-                </Text>
-
-                <TouchableOpacity onPress={showDAtepicker}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <DropDown data={Gender} height={hp(6)} width={wp(39)} />
+              <View>
+                <View style={{ borderWidth: 2, zIndex: 0, borderRadius: 10 }}>
                   <Text
                     style={{
+                      fontSize: 7,
+                      position: 'absolute',
+                      zIndex: 1,
+                      top: -5,
+                      marginStart: 9,
                       color: colors.white,
-                      textAlign: 'center',
-                      textAlignVertical: 'center',
-                      backgroundColor: '#3f3f3f',
-                      padding: 13,
-                      borderStartWidth: 10,
-                      borderRadius: 10,
-                      paddingHorizontal: 30,
-                      paddingVertical: 16,
-                      fontSize: 12,
                     }}>
-                    {date == null ? 'Month/Day/Year' : dateFormat(date)}
+                    Date of birth
                   </Text>
-                </TouchableOpacity>
+
+                  <TouchableOpacity onPress={showDAtepicker}>
+                    <Text
+                      style={{
+                        color: colors.white,
+                        textAlign: 'center',
+                        textAlignVertical: 'center',
+                        backgroundColor: '#3f3f3f',
+                        padding: 13,
+                        borderStartWidth: 10,
+                        borderRadius: 10,
+                        paddingHorizontal: 30,
+                        paddingVertical: 16,
+                        fontSize: 12,
+                      }}>
+                      {date == null ? 'Month/Day/Year' : dateFormat(date)}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
               </View>
             </View>
-          </View>
-          {show && (
-            <DateTimePicker
-              // testID="dateTimePicker"
-              value={new Date}
-              mode={'date'}
-              // is24Hour={true}
-              display="default"
-              onChange={onChange}
+            {show && (
+              <DateTimePicker
+                // testID="dateTimePicker"
+                value={new Date}
+                mode={'date'}
+                // is24Hour={true}
+                display="default"
+                onChange={onChange}
+              />
+            )}
+            <RnButton
+              onPress={() => validation()}
+              fontFamily="light"
+              margin={[20, 0]}
+              title="SIGN UP "
             />
-          )}
-          <RnButton
-            onPress={() => validation()}
-            fontFamily="light"
-            margin={[20, 0]}
-            title="SIGN UP "
-          />
 
-          <View style={styles.footer}>
-            {/* <Icon size={wp(8)}  margin={[0,0,wp(5),0]} source={globalPath.GOOGLE_LOGO} /> */}
-            <ResponsiveText margin={[2, 10]} color={colors.white}>
-              I already have account{' '}
-              <ResponsiveText
-                fontFamily="Bold"
-                color={colors.yellow}
-                onPress={() => navigation.navigate(routeName.LOGIN)}>
-                Sign In
+            <View style={styles.footer}>
+              {/* <Icon size={wp(8)}  margin={[0,0,wp(5),0]} source={globalPath.GOOGLE_LOGO} /> */}
+              <ResponsiveText margin={[2, 10]} color={colors.white}>
+                I already have account{' '}
+                <ResponsiveText
+                  fontFamily="Bold"
+                  color={colors.yellow}
+                  onPress={() => navigation.navigate(routeName.LOGIN)}>
+                  Sign In
+                </ResponsiveText>
               </ResponsiveText>
-            </ResponsiveText>
-            {/* <View style={styles.socialIcon}></View> */}
+              {/* <View style={styles.socialIcon}></View> */}
+            </View>
           </View>
-        </View>
         </ScrollView>
-        </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
       <FlashMessage ref={dropdownRef} />
     </KeyboardAvoidingView>
   );
@@ -377,9 +377,9 @@ const styles = StyleSheet.create({
     // backgroundColor: colors.black,
   },
   screeninfo: {
-  //  flex: 0.9,
+    //  flex: 0.9,
     //justifyContent: 'center',
-    marginVertical:'10%',
+    marginVertical: '10%',
     alignItems: 'center',
   },
   formArea: {
