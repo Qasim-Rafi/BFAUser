76117537fd6741b3051169e10519fc855e059f20
SulemanMirz <sulemug@gmail.com>
2022-01-20 18:15:18 +0500

    bCoin keypad

--- src/screens/Home/Wallet/topUp.js
+++ src/screens/Home/Wallet/topUp.js
@@ -1,6 +1,6 @@
 import { useNavigation, useRoute } from '@react-navigation/native'
 import React, { useState } from 'react'
-import { Image, Pressable, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Image, Pressable, ScrollView, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
 import Icon from '../../../components/Icon'
 import ResponsiveText from '../../../components/RnText'
 import { colors } from '../../../constants/colorsPallet'
@@ -40,7 +40,7 @@ const TopUp = (props) => {
     const topUpAmount = parseFloat(topUpAmountComing).toFixed(2)
 
     const [number, setNumber] = useState(0)
-
+    const [numeric, setNumeric] = useState(false)
 
     // navigation.setOptions({
     //     headerShown:true,
@@ -62,17 +62,20 @@ const TopUp = (props) => {
                     <Text style={{color:'white',fontSize:20}} >Top-up Bcoin Balance</Text>
                     <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }} >
                         <Image source={bCoin} style={{ height: 60, width: 50 }} />
-                        <TextInput
-                            // editable={ number===0 ? false : true }
-                            onPressIn={ ()=> setModalVisible(true)}
-                            style={styles.input}
-                            onChangeText={setNumber}
-                            value={parseFloat(number).toFixed(2)}
-                            maxLength={10}
-                            defaultValue={'' + parseFloat(number) + ''}
-                            keyboardType="numeric"
-                        />
-                        <TouchableOpacity>
+                        <TouchableOpacity onPress={()=>setModalVisible(true)} >
+                            <TextInput
+                                editable={false}
+                                onPressIn={ ()=> setModalVisible(true)}
+                                style={styles.input}
+                                onChangeText={setNumber}
+                                value={parseFloat(number).toFixed(2)}
+                                maxLength={10}
+                                defaultValue={'' + parseFloat(number) + ''}
+                                // keyboardType="numeric"
+                                showSoftInputOnFocus={false}
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=>setNumber(0)} >
                             <Image source={leftArrow} style={{ height: 20, width: 20, margin:5 }} />
                         </TouchableOpacity>
                     </View> 
@@ -150,7 +153,7 @@ const TopUp = (props) => {
 
                     <TouchableOpacity
                         onPress={() => {
-                            navigation.navigate(routeName.WALLET, topUpAmount);
+                            navigation.navigate(routeName.WALLET, number);
                         }}
                         style={{
                             justifyContent: 'center',
@@ -170,49 +173,67 @@ const TopUp = (props) => {
 
                     <Modal isVisible={isModalVisible} animationIn={'slideInUp'} animationOut={'slideOutDown'} backdropOpacity={0} onBackdropPress={()=>setModalVisible(false)} >
                         <View style={styles.centeredModalView} >
-                            <View style={styles.modalView} >
-                            <View style={{ flexDirection: 'row' }} >
-                <TouchableOpacity style={{ alignItems: 'center' }} onPress={() => setNumber(parseFloat(number) + 1)} >
-                    <Image source={_1BCoin} style={styles.image2} />
-                </TouchableOpacity>
-                <TouchableOpacity style={{ alignItems: 'center' }} onPress={() => setNumber(parseFloat(number) + 5)} >
-                    <Image source={_5BCoin} style={styles.image2} />
-                </TouchableOpacity>
-                <TouchableOpacity style={{ alignItems: 'center' }} onPress={() => setNumber(parseFloat(number) + 10)} >
-                    <Image source={_10BCoin} style={styles.image2} />
-                </TouchableOpacity>
-            </View>
-
-            <View style={{ flexDirection: 'row' }} >
-                <TouchableOpacity style={{ alignItems: 'center' }} onPress={() => setNumber(parseFloat(number) + 20)} >
-                    <Image source={_20BCoin} style={styles.image2} />
-                </TouchableOpacity>
-                <TouchableOpacity style={{ alignItems: 'center' }} onPress={() => setNumber(parseFloat(number) + 50)} >
-                    <Image source={_50BCoin} style={styles.image2} />
-                </TouchableOpacity>
-                <TouchableOpacity style={{ alignItems: 'center' }} onPress={() => setNumber(parseFloat(number) + 100)} >
-                    <Image source={_100BCoin} style={styles.image2} />
-                </TouchableOpacity>
-            </View>
-
-            <View style={{ flexDirection: 'row', justifyContent: 'center', width: '100%', marginTop: 20, marginBottom: 20 }} >
-                <TouchableOpacity onPress={() => setNumber(parseFloat(number) + 0.01)} >
-                    <Image source={_1BCent} style={styles.bCents} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => setNumber(parseFloat(number) + 0.05)} >
-                    <Image source={_5BCent} style={styles.bCents} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => setNumber(parseFloat(number) + 0.1)} >
-                    <Image source={_10BCent} style={styles.bCents} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => setNumber(parseFloat(number) + 0.2)} >
-                    <Image source={_20BCent} style={styles.bCents} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => setNumber(parseFloat(number) + 0.5)} >
-                    <Image source={_50BCent} style={styles.bCents} />
-                </TouchableOpacity>
-            </View>
+                            <View style={{flexDirection:'row'}} >
+                                <TouchableOpacity style={[styles.keyBoardSelector,{borderTopLeftRadius:5,borderRightWidth:1}]} onPress={()=>setNumeric(true)} >
+                                    <Text style={{fontSize:30,color:colors.white}} >Numeric</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={[styles.keyBoardSelector,{borderTopRightRadius:5}]} onPress={()=>setNumeric(true)} >
+                                    <Text style={{fontSize:30,color:colors.white}} >Bcoin</Text>
+                                </TouchableOpacity>
                             </View>
+
+                            {numeric ? 
+                            <View style={styles.modalView} >
+                                <View style={{ flexDirection: 'row' }} >
+                                    <TouchableOpacity style={styles.imageContainer} onPress={() => setNumber(parseFloat(number) + 1)} >
+                                        <Image source={_1BCoin} style={styles.image2} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={styles.imageContainer} onPress={() => setNumber(parseFloat(number) + 5)} >
+                                        <Image source={_5BCoin} style={styles.image2} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={styles.imageContainer} onPress={() => setNumber(parseFloat(number) + 10)} >
+                                        <Image source={_10BCoin} style={styles.image2} />
+                                    </TouchableOpacity>
+                                </View>
+
+                                <View style={{ flexDirection: 'row' }} >
+                                    <TouchableOpacity style={styles.imageContainer} onPress={() => setNumber(parseFloat(number) + 20)} >
+                                        <Image source={_20BCoin} style={styles.image2} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={styles.imageContainer} onPress={() => setNumber(parseFloat(number) + 50)} >
+                                        <Image source={_50BCoin} style={styles.image2} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={styles.imageContainer} onPress={() => setNumber(parseFloat(number) + 100)} >
+                                        <Image source={_100BCoin} style={styles.image2} />
+                                    </TouchableOpacity>
+                                </View>
+
+                                <View style={{ flexDirection: 'row',alignItems:'center', justifyContent: 'center', width: '100%', marginTop: 20, marginBottom: 10,alignContent:'center' }} >
+                                    <TouchableOpacity style={styles.imageContainer} onPress={() => setNumber(parseFloat(number) + 0.01)} >
+                                        <Image source={_1BCent} style={styles.bCents} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={styles.imageContainer} onPress={() => setNumber(parseFloat(number) + 0.05)} >
+                                        <Image source={_5BCent} style={styles.bCents} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={styles.imageContainer} onPress={() => setNumber(parseFloat(number) + 0.1)} >
+                                        <Image source={_10BCent} style={styles.bCents} />
+                                    </TouchableOpacity>
+                                </View>
+                                <View style={{ flexDirection: 'row',alignItems:'center', justifyContent: 'center', width: '100%', marginTop: 10, marginBottom: 20 }} >
+                                    <TouchableOpacity style={styles.imageContainer} onPress={() => setNumber(parseFloat(number) + 0.2)} >
+                                        <Image source={_20BCent} style={styles.bCents} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={styles.imageContainer} onPress={() => setNumber(parseFloat(number) + 0.5)} >
+                                        <Image source={_50BCent} style={styles.bCents} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={styles.imageContainer} onPress={() => setModalVisible(false)} >
+                                        <View style={[styles.bCents,{backgroundColor:colors.yellow1, borderRadius:hp(2.2), alignItems:'center', justifyContent:'center'}]} >
+                                            <Text>Done</Text>
+                                        </View>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                                 : null   }
                         </View>
                     </Modal>
 
@@ -256,21 +277,23 @@ const styles = StyleSheet.create({
     },
     modalView: {
         // margin: 20,
-        backgroundColor: colors.grey3,
+        backgroundColor: colors.grey4,
         borderRadius: 10,
         // padding: 35,
-        alignItems: "center",
+        // alignItems: "center",
         shadowColor: "#000",
         // height: '40%',
         // width: '60%',
         borderWidth: 1,
         borderColor: colors.black1,
-        alignSelf: 'center'
+        
+        // alignSelf: 'center'
     },
     bCents: {
-        marginHorizontal: 3,
+        // marginHorizontal: 20,
         height: hp(8.4),
         width: wp(17),
+        alignSelf:'center'
     },
     image2: {
         height: hp(15),
@@ -278,6 +301,17 @@ const styles = StyleSheet.create({
         marginTop: 5,
         borderRadius: 5,
         marginHorizontal: -5,
-        marginVertical: -15
+        marginVertical: -15,
+        alignSelf:'center'
     },
+    imageContainer:{
+        flex:1,
+    },
+    keyBoardSelector:{
+        backgroundColor:colors.lightBlack,
+        // height:,
+        width:'40%',
+        alignItems:'center',
+        
+    }
 })
