26979f1132a8a909397ca825e20236a7c22cc69c
Engwaqar <waqarahmadddd@gmail.com>
2022-02-03 15:39:16 +0500

    PromoAndMenuFixing

--- src/screens/Home/BottomTabs/Promos/PromoBanner.js
+++ src/screens/Home/BottomTabs/Promos/PromoBanner.js
@@ -4,36 +4,39 @@ import ResponsiveText from '../../../../components/RnText'
 import RnButton from '../../../../components/RnButton'
 import Swiper from 'react-native-swiper'
 import { globalPath } from '../../../../constants/globalPath'
-import { promosBannerFakeDATA } from '../../../../constants/mock'
+import {
+    promosBannerFakeDATA,
+    yourFavoriteFakeDATA
+} from '../../../../constants/mock'
 import { colors } from '../../../../constants/colorsPallet'
 import { hp } from '../../../../helpers/Responsiveness'
 const PromosBanner = (props) => {
-    const [data,setData]=React.useState(props.data)
-          console.log(data,'sabbbbb')
+    const [data, setData] = React.useState(props.data)
+    console.log(data, 'sabbbbb')
     return (
-        <ScrollView style={{flexGrow: 1}}>
-		<View style={{flex:1, marginTop:15,}} >
-            
-         <ResponsiveText  color={colors.white}>Promotions</ResponsiveText>
-         <ScrollView style={{flexGrow: 1}}>
-            {promosBannerFakeDATA.map((url, index) => {
-                return (
-                    <View>
-                    <ImageBackground style={styles.Advertisement2ndVarientImage} 
-                    // source={{ uri: url }}
-                    source={url}
+        <ScrollView style={{ flexGrow: 1 }}>
+            <View style={{ flex: 1, marginTop: 15,marginBottom:'40%'}} >
 
-                     >
-                    </ImageBackground>
-                    </View>
-                )
-            })
-			}
-            </ScrollView>
-			</View>
-            </ScrollView>
+                <ResponsiveText color={colors.white}>Promotions</ResponsiveText>
+                <ScrollView style={{ flexGrow: 1 }}>
+                    {yourFavoriteFakeDATA.map((item, index) => {
+                        return (
+                            <View>
+                                <ImageBackground style={styles.Advertisement2ndVarientImage}
+                                    // source={{ uri: url }}
+                                    source={item.url}
 
-     
+                                >
+                                </ImageBackground>
+                            </View>
+                        )
+                    })
+                    }
+                </ScrollView>
+            </View>
+        </ScrollView>
+
+
 
     )
 }
@@ -42,8 +45,8 @@ export default PromosBanner
 
 const styles = StyleSheet.create({
 
-        
-        Advertisement2ndVarientImage: { marginTop:20, justifyContent: 'center',height: hp(18),backgroundColor: 'rgba(0,0,0,0.5)' },
+
+    Advertisement2ndVarientImage: { marginTop: 20, justifyContent: 'center', height: hp(18), backgroundColor: 'rgba(0,0,0,0.5)' },
 
 
 })
