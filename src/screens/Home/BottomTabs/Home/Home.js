80569afd96c843706ebf2de5c2cb1bbc7d9976bf
Saniya Tariq <79018100+saniya384@users.noreply.github.com>
2022-04-12 12:56:21 +0500

    update home

--- src/screens/Home/BottomTabs/Home/Home.js
+++ src/screens/Home/BottomTabs/Home/Home.js
@@ -123,7 +123,7 @@ const Home = ({navigation}) => {
     dispatch(getFavorite(1, 4));
     dispatch(getPromoJobsData(1, 4));
     dispatch(getBfaRecommendations(1, 4));
-    dispatch(getBruneiFoodRewards(1, 8));
+    dispatch(getBruneiFoodRewards(1, 4));
 
     dispatch(getOrders());
     dispatch(GetUserRandomiserSetting());
